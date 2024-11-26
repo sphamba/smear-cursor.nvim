62d4409507e4a9696c1acd8e25904f4fb5909abe
Son Pham-Ba <son.phamba@epfl.ch>
2024-11-27 00:14:48 +0100

    fix getting cursor position in floating windows

--- lua/smear_cursor/screen.lua
+++ lua/smear_cursor/screen.lua
@@ -4,7 +4,18 @@ local M = {}
 
 M.get_screen_cursor_position = function()
 	-- Must be called in a vim.defer_fn, otherwise it will return previous cursor position
-	return vim.fn.screenrow(), vim.fn.screencol()
+	local window_id = vim.api.nvim_get_current_win()
+	local window_info = vim.fn.getwininfo(window_id)[1]
+	local window_config = vim.api.nvim_win_get_config(window_id)
+	local row = vim.fn.screenrow()
+	local col = vim.fn.screencol()
+
+	if #window_config.relative > 0 then
+		row = row + window_info.winrow - 1
+		col = col + window_info.wincol - 1
+	end
+
+	return row, col
 end
 
 
