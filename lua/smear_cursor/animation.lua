a29818d8c000e4eb0d0e454e798c6fc1dc55d782
Son Pham-Ba <son.phamba@epfl.ch>
2025-04-14 17:36:45 +0200

    fix: disappearing character when leaving insert mode on first column

--- lua/smear_cursor/animation.lua
+++ lua/smear_cursor/animation.lua
@@ -366,8 +366,14 @@ M.change_target_position = function(row, col)
 		end
 	end
 
-	if target_position[1] == row and target_position[2] == col and vim.api.nvim_get_mode().mode ~= "i" then return end
-	if target_position[1] == row and vim.api.nvim_get_mode().mode == "c" then return end
+	if
+		(target_position[1] == row and target_position[2] == col and vim.api.nvim_get_mode().mode ~= "i")
+		or (target_position[1] == row and vim.api.nvim_get_mode().mode == "c")
+	then
+		unhide_real_cursor()
+		draw.clear()
+		return
+	end
 	draw.clear()
 
 	target_position = { row, col }
