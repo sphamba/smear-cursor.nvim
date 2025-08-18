sphamba/smear-cursor.nvim

commit b900f1feca8c37479c30889b29327385257443c0

fix: flickering search when buffer scrolls
related to #153

lua/smear_cursor/animation.lua
@@ -426,7 +426,7 @@ M.change_target_position = function(row, col)
 	update_current_ids_and_row()
 
 	if current_window_id == previous_window_id and current_buffer_id == previous_buffer_id then
-		if config.scroll_buffer_space then scroll_buffer_space() end
+		if config.scroll_buffer_space and not vim.api.nvim_get_mode().mode == "c" then scroll_buffer_space() end
 		-- stylua: ignore
 		if
 			(not config.smear_between_neighbor_lines and math.abs(row - current_corners[1][1]) <= 1.5)

commit a1a64940ec3175ff09424baaa6b28a4a2f783074

fix: broken scroll in buffer space
introduced by b900f1f

lua/smear_cursor/animation.lua
@@ -426,7 +426,7 @@ M.change_target_position = function(row, col)
 	update_current_ids_and_row()
 
 	if current_window_id == previous_window_id and current_buffer_id == previous_buffer_id then
-		if config.scroll_buffer_space and not vim.api.nvim_get_mode().mode == "c" then scroll_buffer_space() end
+		if config.scroll_buffer_space and check_smear_outside_cmd_row() then scroll_buffer_space() end
 		-- stylua: ignore
 		if
 			(not config.smear_between_neighbor_lines and math.abs(row - current_corners[1][1]) <= 1.5)