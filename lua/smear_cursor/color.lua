af2e5325760abbdc121ac5a963c8d8ceaa28552a
Son Pham-Ba <son.phamba@epfl.ch>
2026-09-25 13:39:08 +0200

    fix: don't populate empty guicursor with default neovim value

--- lua/smear_cursor/color.lua
+++ lua/smear_cursor/color.lua
@@ -216,7 +216,6 @@ M.unhide_real_cursor = function()
 		if vim.o.guicursor:find(pattern, 1, true) then vim.o.guicursor = vim.o.guicursor:gsub(pattern, "") end
 	end
 
-	if vim.o.guicursor == "" then vim.o.guicursor = vim.api.nvim_get_option_info2("guicursor", {}).default end
 	if vim.o.guicursor == "" then vim.o.guicursor = "a:Cursor" end
 end
 
